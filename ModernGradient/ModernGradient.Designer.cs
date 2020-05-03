031a7bcf675ee5a4d1fe0133223f745e0e05704c
Michael Melikov <coutaq@gmail.com>
2020-05-03 17:54:59 +0300

    Add project files.

---
+++ ModernGradient/ModernGradient.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Coutaq
+{
+    partial class ModernGradient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+       
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ModernGradient1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.ModernGradient_Load);
+            this.Resize += new System.EventHandler(this.ModernGradient_Resize);
+            this.ResumeLayout(false);
+
+        }
+       
+
+        #endregion
+    }
+}
---
+++ ModernGradient/ModernGradient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Coutaq
+{
+    public partial class ModernGradient: UserControl
+    {
+        
+        private Color gradientBegin = Color.White;
+        [
+        Category("Appearance"),
+        Description("Gradient Beginning Color")
+        ]
+        public Color GradientBegin
+        {
+            get { return gradientBegin;}
+            set{
+                gradientBegin = value;
+                Invalidate();
+            }
+        }
+        private Color gradientEnd = Color.Black;
+        [
+        Category("Appearance"),
+        Description("Gradient Ending Color")
+        ]
+        public Color GradientEnd
+        {
+
+            get {return gradientEnd; }
+            set{ 
+                gradientEnd = value;
+                Invalidate();
+            }
+        }
+        public ModernGradient()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void ModernGradient_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            StringFormat style = new StringFormat();
+            style.Alignment = StringAlignment.Near;
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, gradientBegin, gradientEnd, 45F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+        private void ModernGradient_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        [Browsable(false)]
+        public new Color BackColor { get { return Color.White; } }
+        [Browsable(false)]
+        public new Color ForeColor { get { return Color.White; } }
+        [Browsable(false)]
+        public new ImageLayout BackgroundImageLayout { get { return ImageLayout.Center; } }
+        [Browsable(false)] 
+        public new Image BackgroundImage { get { return null; } }
+        [Browsable(false)]
+        public new System.Windows.Forms.RightToLeft RightToLeft { get { return System.Windows.Forms.RightToLeft.Inherit; } }
+        [Browsable(false)]
+        public new Font Font { get { return new Font(Font.SystemFontName, 8); } }
+        public new System.Windows.Forms.DockStyle Dock { get { return System.Windows.Forms.DockStyle.Fill; } }
+
+    }
+
+}
